9d055ab60bc510baea54fa77001d98786a3b0a05
Ruben Angeles <teckever6@hotmail.com>
2021-03-16 00:49:07 -0600

    Se Agrega Comando Ping

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Token = process.env.TOKEN;
+const { prefix, token } = require("./config.json");
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
+//Escuchando mensajes
+client.on("message", function (message) {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
 
-client.login(config.token);
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
+
+	switch (command) {
+
+		case "ping":
+			const timeTaken = Date.now() - message.createdTimestamp;
+			message.channel.send({
+				embed: {
+					color: 1752220,
+					description: `${timeTaken} ms.`
+				}
+			});
+			break;
+	}
+
+});
+
+
+
+client.login(token); //Coneccion a Discord
