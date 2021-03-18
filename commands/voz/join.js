1796f922ad0795e85a6c55e7c49b7ba1c7c442eb
Ruben Angeles <teckever6@hotmail.com>
2021-03-18 15:09:36 -0600

    Se mejora look del mensaje en discord

--- commands/voz/join.js
+++ commands/voz/join.js
@@ -2,7 +2,14 @@ module.exports = {
     name: "join",
     description: "Conecta el bot a tu canal de voz",
     execute(message) {
-        if (!message.member.voice.channel) return message.channel.send("Debes estar conectado a un canal de voz!"); 
+        if (!message.member.voice.channel) {
+            return message.channel.send({
+				embed: {
+					color: "FF0000",
+					description: "Tienes que estar conectado a un canal de Voz"
+				}
+			});
+        } 
         message.member.voice.channel.join();
     }
 }
--- commands/voz/leave.js
+++ commands/voz/leave.js
@@ -2,7 +2,14 @@ module.exports = {
     name: "leave",
     description: "Desconecta el bot del canal de voz",
     execute(message) {
-        if (!message.guild.me.voice.channel) return message.channel.send("No estoy conectado a ningun canal de voz");
+        if (!message.guild.me.voice.channel){
+            return message.channel.send({
+				embed: {
+					color: "FF0000",
+					description: "No estoy conectado a nungun canal de voz"
+				}
+			});
+        } 
         message.guild.me.voice.channel.leave();
     }
 }
